bf19047f83d4130a02ca12f068de04b735b2a972
tunaingot <68312199+tunaingot@users.noreply.github.com>
2026-04-06 10:38:22 +0900

    Update blink.py

--- LChika/blink.py
+++ LChika/blink.py
@@ -1,14 +1,19 @@
 from machine import Pin
 from utime import sleep
 
-pin = Pin("LED", Pin.OUT)
+led = Pin("LED", Pin.OUT)
 
 print("LED starts flashing...")
 while True:
     try:
-        pin.toggle()
+        # pin.toggle()
+        state = led.value()
+        print(state)
+        state = not state
+        print(state)
+        led.value(state)
         sleep(1) # sleep 1sec
     except KeyboardInterrupt:
         break
-pin.off()
+led.off()
 print("Finished.")
